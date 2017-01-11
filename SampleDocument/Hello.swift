94ccefc17ad6ee150eeac2068e284d92cb1a48a8
somkiat <somkiat.p@gmail.com>
2017-01-11 11:39:55 +0700

    Sample project

---
+++ SampleDocument/Hello.swift
@@ -0,0 +1,37 @@
+//
+//  Hello.swift
+//  SampleDocument
+//
+//  Created by somkiat puisungnoen on 1/11/2560 BE.
+//  Copyright © 2560 ___UP1___. All rights reserved.
+//
+
+import Foundation
+
+/// Chill Chill with Hello
+public class Hello {
+    
+    // MARK: Public methods
+    
+    /**
+     Say hi to the world
+     
+     - parameter name:  Name of user
+     */
+    
+    public func sayHi(name: String) {
+        
+    }
+    
+    // MARK: Private methods
+    
+    /**
+     Formatter of data
+     
+     - parameter data: Firtname and Lastname
+     */
+    private func formatData(data: String) {
+        
+    }
+    
+}
